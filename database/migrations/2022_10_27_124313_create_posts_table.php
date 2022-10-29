6fb1089955d657865032a46eb74d0ae232bf1ecf
brothersmedia190 <brothersmedia190@gmail.com>
2022-10-29 05:26:18 +0200

    ok1

---
+++ database/migrations/2022_10_27_124313_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->text('photo');
+            $table->text('heading');
+            $table->text('short_content');
+            $table->text('content');
+            $table->integer('total_view');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
